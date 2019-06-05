3d14b81d081343c477fe9949e708813818464262
Chinh Le <chinh.le11@gmail.com>
2019-06-05 06:15:39 -0400

    next.js & components & pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import User from '../../components/User';
+
 class AuthIndexPage extends Component {
     render() {
         return (
             <div>
                 <h1>The Auth Index Page</h1>
+                <User name="Max" age="30" />
             </div>
         );
     };
